abb5517225aad729119d8cb711035d5402530976
Junzhe Zhang <jzh.zhang@hotmail.com>
2022-04-27 21:09:46 +0800

    1 of array

--- array/1.py
+++ array/1.py
@@ -14,10 +14,30 @@ x = Solution()
 x.twoSum(nums, target)
 
 
-# Solution
+# Solution 1
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+    def twoSum(self, nums, target):
         for i in range(len(nums)):
             for j in range(i + 1, len(nums)):
                 if nums[j] == target - nums[i]:
                     return [i, j]
+
+
+# Solution 2
+class Solution:
+    def twoSum(self, nums, target):
+        hashmap = {nums[i]: i for i in range(len(nums))}
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in hashmap and hashmap[complement] != i:
+                return [i, hashmap[complement]] 
+
+# Solution 3
+class Solution:
+    def twoSum(self, nums, target):
+        hashmap = {}
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in hashmap:
+                return [i, hashmap[complement]]
+            hashmap[nums[i]] = i
