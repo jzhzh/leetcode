e73d49673731a6f77368cc410059d494b8647600
Junzhe Zhang <jzh.zhang@hotmail.com>
2022-04-27 20:17:42 +0800

    1 of array

--- array/1.py
+++ array/1.py
@@ -1,3 +1,4 @@
+# Self
 class Solution:
     def twoSum(self, nums, target):
         for index, i in enumerate(nums):
@@ -10,4 +11,13 @@ class Solution:
 nums = [2,7,11,15]
 target = 9
 x = Solution()
-x. twoSum(nums, target)
+x.twoSum(nums, target)
+
+
+# Solution
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[j] == target - nums[i]:
+                    return [i, j]
